bbddc3b6d90015c1bf2b29296ffdb2e1341f4d67
mahmud035 <mahamudulhasan4148@gmail.com>
2025-12-05 10:39:01 +0600

    Update

--- cluster0.mongodb/sample_training/companies_query.js
+++ cluster0.mongodb/sample_training/companies_query.js
@@ -1,7 +1,5 @@
 // The following query filter to match documents that either contain a description field with a null value or do not contain the description field:
-// db.companies
-//     .find({ description: null })
-//     .project({ description: 1 })
+db.companies.find({ description: null }).project({ description: 1 });
 
 // The following query filter to match only documents that do not contain the description field. Only the document that you inserted earlier should appear:
 db.companies.find({ description: { $exists: false } });
